5a04f3cbcbb34960992d329a0478c969b979cc9b
vnugopal <venugopalnarayanabhatla@Venugopals-MacBook-Pro.local>
2015-10-30 12:51:09 -0700

    Checkpoint 10 Alcator

---
+++ Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Venugopal Narayanabhatla on 10/30/15.
+//  Copyright © 2015 Venugopal Narayanabhatla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
---
+++ Alcolator/MainMenuViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainMenuViewController.m
+//  Alcolator
+//
+//  Created by Venugopal Narayanabhatla on 10/30/15.
+//  Copyright © 2015 Venugopal Narayanabhatla. All rights reserved.
+//
+
+#import "MainMenuViewController.h"
+
+@interface MainMenuViewController ()
+
+@end
+
+@implementation MainMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
